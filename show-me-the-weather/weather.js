df4b7f3465afca52a9eb8c3c1c9ef3450d6fbb1f
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2017-06-03 22:06:07 -0300

    Missing temperature unit change

--- show-me-the-weather/weather.js
+++ show-me-the-weather/weather.js
@@ -1,5 +1,14 @@
-var celsus = false;
-var temperature;
+var celsus = true;
+var temperature = {};
+
+function temperatureUpdate(){
+  if (celsus){
+    $("#temperature").html(temperature.c);
+  }else{
+    $("#temperature").html(temperature.f);
+  }
+
+}
 
 $(document).ready(function() {
 
@@ -14,16 +23,20 @@ $(document).ready(function() {
     navigator.geolocation.getCurrentPosition(function(position) {
       console.log("latitude: " + position.coords.latitude + "<br>\nlongitude: " + position.coords.longitude);
 
-    $.getJSON( "https://crossorigin.me/https://api.darksky.net/forecast/510b9e1db346eca8acb7dd594fa98282/"+position.coords.latitude+","+position.coords.longitude, function( weather ) {
-      // Help about this api: https://darksky.net/dev/docs/forecast
-      // And: https://darksky.net/dev/docs/response
-      temperature = weather['currently']["temperature"];
-      console.log("Temperature: " + weather['currently']["temperature"]);
-      console.log("icon: " + weather['currently']["icon"]);
-      // $("#temp").html(weather['currently']["temperature"]);
-      // $("#weather").className = weather['currently']["icon"];
-      document.getElementById("weather").className =  weather['currently']["icon"];
-    });
+      $.getJSON( "https://crossorigin.me/https://api.darksky.net/forecast/510b9e1db346eca8acb7dd594fa98282/"+position.coords.latitude+","+position.coords.longitude +"?units=si", function( weather ) {
+        // Help about this api: https://darksky.net/dev/docs/forecast
+        // And: https://darksky.net/dev/docs/response
+        temperature.c = weather['currently']["temperature"];
+        // T(°F) = T(°C) × 9/5 + 32
+        temperature.f = temperature.c*1.8 + 32;
+        console.log("Temperature: " + temperature.c);
+        console.log("Temperature: " + temperature.f);
+        console.log("icon: " + weather['currently']["icon"]);
+        temperatureUpdate();
+
+        // $("#weather").className = weather['currently']["icon"];
+        document.getElementById("weather").className =  weather['currently']["icon"];
+      });
 
     });
 
