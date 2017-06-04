dac3129b6a5d6cc27e79861e933b349061d80add
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2017-06-03 21:19:20 -0300

    The icon is working

--- show-me-the-weather/weather.js
+++ show-me-the-weather/weather.js
@@ -10,13 +10,19 @@ $(document).ready(function() {
 
 
   if (navigator.geolocation) {
+
     navigator.geolocation.getCurrentPosition(function(position) {
       console.log("latitude: " + position.coords.latitude + "<br>\nlongitude: " + position.coords.longitude);
 
     $.getJSON( "https://crossorigin.me/https://api.darksky.net/forecast/510b9e1db346eca8acb7dd594fa98282/"+position.coords.latitude+","+position.coords.longitude, function( weather ) {
+      // Help about this api: https://darksky.net/dev/docs/forecast
+      // And: https://darksky.net/dev/docs/response
       temperature = weather['currently']["temperature"];
       console.log("Temperature: " + weather['currently']["temperature"]);
-      $("#temp").html(weather['currently']["temperature"]);
+      console.log("icon: " + weather['currently']["icon"]);
+      // $("#temp").html(weather['currently']["temperature"]);
+      // $("#weather").className = weather['currently']["icon"];
+      document.getElementById("weather").className =  weather['currently']["icon"];
     });
 
     });
